97d6e22c363b4d4ab621147f1c36d0cafcdc6ffe
anranyao <anranyao@umich.edu>
2022-11-28 16:06:34 -0500

    edit test that

--- tests/testthat/test-linear.R
+++ tests/testthat/test-linear.R
@@ -1,3 +1,4 @@
 test_that("linear works", {
+  cc<-Depression ~ conFatalism + Sex + R_E + factor(Age1)
   expect_equal(as.numeric(linear(cc,mydata)$F.value),as.numeric(summary(lm(cc,data=mydata))$fstatistic[1]))
 })
