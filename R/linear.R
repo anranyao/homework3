86cac9e5a4b18179f950a3d83952bd5d4fe31e47
anranyao <anranyao@umich.edu>
2022-11-29 01:14:59 -0500

    updates

--- R/linear.R
+++ R/linear.R
@@ -1,3 +1,48 @@
+#'linear: Fitting Simple Linear Regression Models
+#'
+#'`linear` is used to fit simple linear regression models. And it can also returns 95% confidence intervals for one or more parameters in a fitted model.
+#'
+#'@param formula.lm an object of class "formula" (or one that can be coerced to that class): a symbolic description of the model to be fitted. The details of model specification are given under ‘Details’.
+#'@param data an optional data frame, list or environment (or object coercible by \code{\link{as.data.frame}} to a data frame) containing the variables in the model. If not found in data, the variables are taken from environment(formula), typically the environment from which linear is called.
+#'
+#'@return linear returns a list containing at least the following components:
+#'\describe{
+#'\item{residuals.descript}{The desciption of residuals(minimum, 1st quartile, median, 3rd quartile, maximum)}
+#'\item{Coefficients}{a p x 4 matrix with columns for the estimated coefficient, its standard error, t-statistic and corresponding (two-sided) p-value. Aliased coefficients are omitted.}
+#'\item{CI}{95% confidence intervals for parameters in the fitted model.}
+#'\item{residual.std.error}{the square root of the estimated variance of the random error σ^2 = 1/(n-p) Sum(w[i] R[i]^2),where R[i] is the i-th residual, residuals[i].}
+#'\item{residual.df}{the residual degrees of freedom.}
+#'\item{R_squared}{R^2, the ‘fraction of variance explained by the model’,R^2 = 1 - Sum(R[i]^2) / Sum((y[i]- y*)^2),where y* is the mean of y[i] if there is an intercept and zero otherwise.}
+#'\item{Adjusted.R_squared}{the above R^2 statistic ‘adjusted’, penalizing for higher p.}
+#'\item{F.value}{}
+#'\item{df}{degrees of freedom of F statistic.}
+#'\item{pvalue.f}{P value of F statistic.}
+#'\item{fittedvalues}{the fitted mean values.}
+#'\item{residual}{the residuals, that is response minus fitted values.}
+#'}
+#'
+#'@examples
+#'attach(mydata) #using the data
+#'cc<-Depression ~ conFatalism + Sex + R_E + factor(Age1)
+#'results<-linear(cc,mydata)
+#'coef_c<-results$Coefficients # the coefficients and its related values.
+#'coef_c
+#'results$F.value
+#'## input"formula.lm" must be a formula.
+#' \donttest{
+#' attach(mydata)
+#'    linear(Sex,mydata)
+#' }
+#'
+#'@export
+#'
+#'@details Models for linear are specified symbolically. A typical model has the form `response ~ terms` where `response` is the (numeric) response vector and `terms` is a series of terms which specifies a linear predictor for `response`.
+#'
+#'A formula has an implied intercept term. To remove this use either `y ~ x - 1` or `y ~ 0 + x`.
+#'
+#'@references LM: Fitting linear models (1969) RDocumentation. R-core R-core@R-project.org. Available at: https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/lm (Accessed: November 20, 2022).
+
+
 linear <- function(formula.lm, data) {
   if (class(formula.lm) != "formula")
     stop("'formula.lm' must be a formula")
@@ -39,7 +84,6 @@ linear <- function(formula.lm, data) {
   residual.descript <- fivenum(residual)
   return(
     list(
-      formula.lm,
       residuals.descript = residual.descript,
       Coefficients = coefficient,
       CI = CI,
